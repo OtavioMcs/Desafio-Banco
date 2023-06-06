2743ca43288a74c3721ded02906e27a7a0824976
OtavioMcs <130837229+OtavioMcs@users.noreply.github.com>
2023-06-05 21:03:18 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,144 +1,103 @@
-package JogoDaVelha;
-
 import java.util.Scanner;
-public class Main {
 
-    public static void exibeTabuleiro(char[][] tabuleiro){
-        System.out.println("  -------------------");
-        for (int i = 0; i < tabuleiro.length; i++) {
-            System.out.print("  |  ");
-            for (int x = 0; x < tabuleiro[i].length; x++) {
-                System.out.print(tabuleiro[i][x] + "  |  ");
-            }
-            System.out.println("\n  -------------------");
-        }
+public class DesafioBanco {
+
+    public static void infoClient(String nome, String conta, double saldo){
+        System.out.println("******************************************");
+        System.out.println("Informações do Cliente");
+        System.out.println("Nome:               " + nome);
+        System.out.println("Tipo de Conta:      " + conta);
+        System.out.println("Saldo inicial:      " + saldo);
+        System.out.println("******************************************");
         return;
     }
 
     public static void mostraMenu(){
-        System.out.print("""
-                MENU 
+        int menuNumber;
+        Scanner input = new Scanner(System.in);
+        String menu = """
+                Operações
                 
-                1 - Jogo da Velha
-                2 - Sair o/
-                """);
+                1 - Consultar Saldo
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair o/
+                
+                Digite a opção desejada.""";
+        System.out.println(menu);
         return;
     }
 
     public static void limpa(){
-        for (int y = 0; y <= 6; y++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println("\n");
         }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        char[][] tabuleiro = new char[3][3];
-        char sinal;
-        boolean ganhador = false;
-        int vez, linha, coluna, menu = 0;
+        String nome, conta;
+        double saldo = 0, valor = 0;
+        int menu = 0;
 
+        System.out.println("Insira seu nome. ");
+        nome = input.nextLine();
+        System.out.println("Insira seu tipo de conta. ");
+        conta = input.nextLine();
+        System.out.println("Insira seu saldo inicial. ");
+        saldo = input.nextDouble();
 
-        while(menu != 2) {
+        limpa();
 
-            ganhador = false;
-            vez = 1;
-            linha = 0;
-            coluna = 0;
-            for (int i = 0; i < tabuleiro.length; i++) {
-                for (int j = 0; j < tabuleiro[i].length; j++){
-                    tabuleiro[i][j] = ' ';
-                }
-            }
+        infoClient(nome, conta, saldo);
+        mostraMenu();
+        menu = input.nextInt();
 
-            mostraMenu();
-            menu = input.nextInt();
-            limpa();
+        while (menu != 4){
+            switch (menu){
+                case 1:
+                    System.out.println("O seu saldo é " + saldo + "\n");
+                    mostraMenu();
+                    menu = input.nextInt();
+                    break;
 
-            if(menu == 1) {
-                while (!ganhador) {
-
-                    System.out.println("O primeiro jogador é: X.");
-                    System.out.println("O segundo jogador é: O.");
-
-                    if (!(vez != 1)) {
-                        exibeTabuleiro(tabuleiro);
-                    }
-
-                    if (vez % 2 == 1) {
-                        System.out.println("Vez do jogador 1, escolha a linha e coluna (1-3). ");
-                        sinal = 'X';
-                    } else {
-                        System.out.println("Vez do jogador 2, escolha a linha e coluna (1-3). ");
-                        sinal = 'O';
-                    }
-
-                    boolean linhaJogavel = false;
-                    while (!linhaJogavel) {
-                        System.out.println("Entre com a linha 1, 2 ou 3. ");
-                        linha = input.nextInt();
-                        if (linha >= 1 && linha <= 3) {
-                            linhaJogavel = true;
-                        } else {
-                            System.out.println("Linha não disponível. Tente novamente. ");
+                case 2:
+                    System.out.println("Qual o valor que será recebido? ");
+                    valor = input.nextDouble();
+                    if(valor <= 0){
+                        while(!(valor != 0)) {
+                            System.out.println("Número inválido, tente novamente.");
+                            valor = input.nextDouble();
                         }
                     }
+                    saldo += valor;
+                    System.out.println("Saldo atualizado para " + saldo + "\n");
+                    mostraMenu();
+                    menu = input.nextInt();
+                    break;
 
-                    boolean colunaJogavel = false;
-                    while (!colunaJogavel) {
-                        System.out.println("Entre com a coluna 1, 2 ou 3. ");
-                        coluna = input.nextInt();
-                        if (coluna >= 1 && coluna <= 3) {
-                            colunaJogavel = true;
-                        } else {
-                            System.out.println("Coluna não disponível. Tente novamente. ");
+                case 3:
+                    System.out.println("Qual o valor que será transferido? ");
+                    valor = input.nextDouble();
+                    if(valor > saldo){
+                        while(valor > saldo) {
+                            System.out.println("O valor a ser transferido não disponível para tal atividade. Tente novamente. ");
+                            valor = input.nextDouble();
                         }
                     }
+                    saldo -= valor;
+                    System.out.println("Saldo atualizado para " + saldo + "\n");
+                    mostraMenu();
+                    menu = input.nextInt();
+                    break;
 
-                    linha -= 1;
-                    coluna -= 1;
-
-                    limpa();
-
-                    if (tabuleiro[linha][coluna] == 'X' || tabuleiro[linha][coluna] == 'O') {
-                        System.out.println("Posição já usada. Tente novamente. ");
-                    } else {
-                        tabuleiro[linha][coluna] = sinal;
-                        vez += 1;
-                    }
-
-                    exibeTabuleiro(tabuleiro);
-
-                    if ((tabuleiro[0][0] == 'X' && tabuleiro[1][1] == 'X' && tabuleiro[2][2] == 'X') ||
-                            (tabuleiro[2][0] == 'X' && tabuleiro[1][1] == 'X' && tabuleiro[0][2] == 'X') ||
-                            (tabuleiro[0][0] == 'X' && tabuleiro[0][1] == 'X' && tabuleiro[0][2] == 'X') ||
-                            (tabuleiro[1][0] == 'X' && tabuleiro[1][1] == 'X' && tabuleiro[1][2] == 'X') ||
-                            (tabuleiro[2][0] == 'X' && tabuleiro[2][1] == 'X' && tabuleiro[2][2] == 'X') ||
-                            (tabuleiro[0][0] == 'X' && tabuleiro[1][0] == 'X' && tabuleiro[2][0] == 'X') ||
-                            (tabuleiro[0][1] == 'X' && tabuleiro[1][1] == 'X' && tabuleiro[2][1] == 'X') ||
-                            (tabuleiro[0][2] == 'X' && tabuleiro[1][2] == 'X' && tabuleiro[2][2] == 'X')) {
-                        ganhador = true;
-                        System.out.println("Vitória do jogador 1!");
-                        System.out.println("********************* \n");
-                    } else if ((tabuleiro[0][0] == 'O' && tabuleiro[1][1] == 'O' && tabuleiro[2][2] == 'O') ||
-                            (tabuleiro[2][0] == 'O' && tabuleiro[1][1] == 'O' && tabuleiro[0][2] == 'O') ||
-                            (tabuleiro[0][0] == 'O' && tabuleiro[0][1] == 'O' && tabuleiro[0][2] == 'O') ||
-                            (tabuleiro[1][0] == 'O' && tabuleiro[1][1] == 'O' && tabuleiro[1][2] == 'O') ||
-                            (tabuleiro[2][0] == 'O' && tabuleiro[2][1] == 'O' && tabuleiro[2][2] == 'O') ||
-                            (tabuleiro[0][0] == 'O' && tabuleiro[1][0] == 'O' && tabuleiro[2][0] == 'O') ||
-                            (tabuleiro[0][1] == 'O' && tabuleiro[1][1] == 'O' && tabuleiro[2][1] == 'O') ||
-                            (tabuleiro[0][2] == 'O' && tabuleiro[1][2] == 'O' && tabuleiro[2][2] == 'O')) {
-                        ganhador = true;
-                        System.out.println("Vitória do jogador 2!");
-                        System.out.println("********************* \n");
-                    } else if (vez > 9) {
-                        ganhador = true;
-                        System.out.println("Houve um empate.");
-                        System.out.println("********************* \n");
-                    }
-                }
+                default:
+                    System.out.println("Opção inválida. Tente novamente. \n");
+                    mostraMenu();
+                    menu = input.nextInt();
             }
+
         }
-        System.out.println("Muito obrigado por jogar! big beijo.");
+        System.out.println("Programa Finalizado! Obrigado, " + nome + "!");
     }
 }
